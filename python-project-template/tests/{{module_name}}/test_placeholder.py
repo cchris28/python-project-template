a93fbb8dd81e6828aa99c29b2956919346e16dc8
Carl Christofferson <cchris28@uw.edu>
2023-04-15 11:40:41 -0700

    fixes

--- python-project-template/tests/{{module_name}}/test_placeholder.py
+++ python-project-template/tests/{{module_name}}/test_placeholder.py
@@ -1,4 +1,5 @@
 """Tests that exist as placeholders. You can delete them, rename them, or use them as examples."""
 
 def test_placeholder_exists() -> None:
+    """Always passes"""
     assert True
